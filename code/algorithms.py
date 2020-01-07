9fc65b1704b89ee8d1ed505528d6d61763696a8f
Sjopia <leonbesseling@gmail.com>
2020-01-07 18:59:52 +0100

    Pseudo-pseudocode for heuristics

--- code/algorithms.py
+++ code/algorithms.py
@@ -1,46 +1,27 @@
-from objects import Car
-import csv
 import random
 
-boardsize = 9
-cars = {}
-colors = ['blue_1', 'yellow_1', 'green_1', 'dark_green', 'deep_pink_1a', 'dark_orange']
 
-# read cars from file
-with open('./gameboards/Rushhour9x9_5.csv') as file:
-    # skip first line, and read rest of file
-    next(file)
-    text = csv.reader(file)
+def random():
+    car = random.choice(list(cars.values()))
+    if car.front_distance > 0 or car.rear_distance > 0: ## IN EEN REGEL ???? OF MET ELIF ????
+        RushHour.move(car)
+    elif car.rear_distance > 0:
+        RushHour.move(car)
 
-    # loop over lines in file and adjust values for use in Car object
-    for i, line in enumerate(text):
-        position = (boardsize - int(line[3].strip().strip('\"')), int(line[2].strip().strip('\"')) - 1)
 
-        # assign the right color to the main car
-        if line[0] == 'X':
-            color = 'red_1'
-        else:
-            color = colors[i%len(colors)]
-
-        # create a list of cars on the board
-        cars[line[0]] = Car(line[0], line[1].strip(), position, color, int(line[4].strip()))
-
-# RANDOM
-car = random.choice(list(cars.values()))
-# if car.blockfront:
-    # Rush.move(car)
-# elif car.blockback:
-    # Rush.move(car)
-
-# OBSTACLE CHAIN
-leader = cars['X']
-if leader.frontdistance > 0:
-    Rush.move(leader)
-else:
-    obstacle = Matrix(leader.position + leader.length) # should be a car object
-    while not Rush.move(obstacle):
-        obstaclefront = Matrix(obstacle.position + obstacle.length)
-        obstacleback = Matrix(obstacle.position + obstacle.length)
-
+def obstacle_chain():
+    leader = cars['X']
+    if leader.front_distance > 0:
+        RushHour.move(leader)
+    else:
+        obstacle = RushHour.matrix[leader.row][leader.col + leader.length] # should be a car object
+        while not RushHour.move(obstacle):
+            if obstacle.direction == "H":
+                car_front = RushHour.matrix[obstacle.row][obstacle.col + obstacle.length]
+                car_rear = RushHour.matrix[obstacle.row][obstacle.col - 1]
+            elif obstacle.direction == "V":
+                car_front = RushHour.matrix[obstacle.row + obstacle.length][obstacle.col]
+                car_rear = RushHour.matrix[obstacle.row - 1][obstacle.col]
+            obstacle = random.choice([car_front, car_rear])
 
 
