86a1cf8d455ffa670c7e5740651d5d71980942d4
Jordy Schifferstein <jordyschifferstein@hotmail.com>
2020-01-10 16:36:46 +0100

    Updated modes

--- code/modes/test.py
+++ code/modes/test.py
@@ -2,17 +2,17 @@ import os
 from code.algorithms import random
 
 
-def test(RushHour):
+def test(RushHour, algorithm):
     steps = 0
 
-    while not rush.game_won(steps):
+    while not RushHour.game_won(steps):
         steps += 1
 
         if to_print == 'yes':
             os.system('cls')
-            rush.printboard()
+            RushHour.printboard()
 
         if algorithm == '1':
-            random.random_pure(rush)
+            random.random_pure(RushHour)
         elif algorithm == '2':
-            random.random_constraint(rush)
+            random.random_constraint(RushHour)
--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from code.algorithms import random
-from code.classes import board
-from code.modes import *
 from math import sqrt
+import os
 from sys import exit, argv
 from time import sleep
 
-import matplotlib.pyplot as plt
-import numpy as np 
-import scipy.stats as stats
-from scipy.stats import norm
-import pylab as pl
-import statistics
-import pandas
-from collections import Counter
-
-import chart_studio.plotly
-
-
-
-import os
+from code.classes import board
+from code.modes import manual, plot, test
 
 
 if __name__ == '__main__':
@@ -42,34 +28,11 @@ if __name__ == '__main__':
         to_print = input('Do you want to print? (yes, no): ')
 
     board_path = f'data/{argv[1]}'
-    rush = board.RushHour(board_path)
+    RushHour = board.RushHour(board_path)
 
     if mode == 'manual':
-        steps = 0
-        while not rush.game_won(steps):
-            os.system('cls')
-            rush.printboard()
-            print('Moves:', steps)
-            car_to_move = input('Which car do you want to move? ').upper()
-
-            if not car_to_move.upper() in rush.cars.keys():
-                print('Invalid car!')
-                sleep(1)
-                continue
-
-            try:
-                distance = int(input('How far? '))
-            except ValueError:
-                print('Invalid distance!')
-                sleep(1)
-                continue
-
-            if not rush.move(rush.cars[car_to_move], distance):
-                print('Invalid move!')
-                sleep(1)
-            else:
-                steps += 1
+        manual.manual(RushHour)
     elif mode == 'plot':
-        plot.plot(rush)
+        plot.plot(RushHour, algorithm, board_path)
     else:
-        test.test
+        test.test(RushHour, algorithm)
