e1888085bd721bde3e0f5a88ebd8ccc4b9b896eb
Yassin <yassin.el-baz@student.uva.nl>
2020-01-28 15:22:20 +0100

    jordi plot

--- code/algorithms/branch_bound.py
+++ code/algorithms/branch_bound.py
@@ -12,20 +12,20 @@ class BranchAndBound(dfs):
     def run(self):
         """Runs the branch and bound algorithm. If a solution is not found within
             the initial depth, this limit is increased by 5. If a solution is found, 
-            depth is lowered by 1 until no better solution can be found.
+            depth is lowered by 1 until no better solution can be found. 
         """
         depth = 15
+
         while True:
             solution = self.solution
             print("Now searching to depth:", depth)
-            
+
             if self.search_to_layer(depth):
                 depth = len(self.solution) - 1
             else:
                 depth += 5
             
             # The algorithm can stop if the length of 
-    
             if len(self.solution) and len(self.solution) == len(solution):
                 print(self.solution)
                 break
